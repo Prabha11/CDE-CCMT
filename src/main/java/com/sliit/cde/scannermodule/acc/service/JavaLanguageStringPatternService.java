e2b028b8b383e26cc76975d153782c77779cca3e
Prabhath Bandara <okprabhath@gmail.com>
2020-05-17 13:07:20 +0530

    new analyzers java string pattern services IV cleaned

--- src/main/java/com/sliit/cde/scannermodule/acc/service/JavaLanguageStringPatternService.java
+++ src/main/java/com/sliit/cde/scannermodule/acc/service/JavaLanguageStringPatternService.java
@@ -33,7 +33,6 @@ public class JavaLanguageStringPatternService extends StringPatternService {
                 "(\\s+|^)(byte|short|int|long|float|double|boolean|char)(\\s+)(.*?);");
         Matcher textInsideDoubleQuotedMatcher = textInsideDoubleQuoted.matcher(codeLine);
         boolean r = textInsideDoubleQuotedMatcher.find();
-//        if (r) System.out.println(textInsideDoubleQuotedMatcher.group(0));
         return r;
     }
 
@@ -45,35 +44,5 @@ public class JavaLanguageStringPatternService extends StringPatternService {
         boolean isPrimitive = isSubString(parameter, "byte|short|int|long|float|double|boolean|char");
 
         return !isArray && isPrimitive;
-    }
-
-    public static void maind(String[] args) {
-        JavaLanguageStringPatternService javaLanguageStringPatternService = new JavaLanguageStringPatternService();
-        System.out.println(javaLanguageStringPatternService.getReturnVariableTypeOfMethod("    public String index() {\n"));
-        System.out.println(javaLanguageStringPatternService.getCodeInsideBrackets("    public String index() {\n"));
-        System.out.println(javaLanguageStringPatternService.getNumberOfPrimitiveParameters(javaLanguageStringPatternService.getCodeInsideBrackets("    public String index() {\n")));
-        System.out.println(javaLanguageStringPatternService.getNumberOfCompositeParameters(javaLanguageStringPatternService.getCodeInsideBrackets("    public String index() {\n")));
-        System.out.println(javaLanguageStringPatternService.isVoidDataType(javaLanguageStringPatternService.getReturnVariableTypeOfMethod("    public String index() {\n")));
-//        System.out.println(stringPatternService.isMethod("private int getInt(int pm, String i){\n"));
-
-    }
-
-    public static void main(String[] args) {
-        String codeLine = "  public static void main  (@ano String[ ] args, @ano String[] args, @ano String args, @ano int args, int[] args, int args, int args, long args, float args, char args,) {\n";
-        JavaLanguageStringPatternService javaLanguageStringPatternService = new JavaLanguageStringPatternService();
-
-        System.out.println(javaLanguageStringPatternService.getNumberOfPrimitiveParameters(javaLanguageStringPatternService.getCodeInsideBrackets(codeLine)));
-        System.out.println(javaLanguageStringPatternService.getNumberOfCompositeParameters(javaLanguageStringPatternService.getCodeInsideBrackets(codeLine)));
-
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("@ano String[ ] args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("@ano String[] args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("@ano String args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("@ano int args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter(" int[] args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter(" int args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("int args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("long args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("float args"));
-        System.out.println(javaLanguageStringPatternService.isPrimitiveParameter("char args"));
     }
 }
